9899a5e4b3e3bd326f0de99c5d3abc36a7eed7b8
strixzy14 <165613459+strixzy14@users.noreply.github.com>
2026-04-10 18:11:50 +0700

    Update fpsaimbot.lua

--- fpsaimbot.lua
+++ fpsaimbot.lua
@@ -1,3 +1,3 @@
-มึงจะ skid ควยไร thi
+-- [ มึงจะ skid ควยไร thi ]
 
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/e760ca762acbbffe4ca253f68ce5cffc2d662702368b70e0785c27f18790d3af/download"))()
